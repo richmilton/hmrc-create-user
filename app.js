dcc2f5ae3ba9a70b25fab613e227e7524b7a3ffe
richard <richardmilton@ahotclub.com>
2019-02-19 23:33:06 +0000

    add license

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 /*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 require('dotenv').config();
@@ -6,20 +17,20 @@ const request = require('superagent');
 const express = require('express');
 const winston = require('winston');
 
-// Start Client configuration
 const serverToken = process.env.HMRC_SERVER_TOKEN;
 const port = process.env.PORT;
 const apiBaseUrl = process.env.HMRC_BASE_URL;
+const hmrcCreateUserPath = 'create-test-user';
+const appCreateUserEndpoint = '/createTestUser';
 
-const endpoints = {
-  individual: 'create-test-user/individuals',
-  organisation: 'create-test-user/organisations',
-  agent: 'create-test-user/agents'
+const hmrcEndpoints = {
+  individual: `${hmrcCreateUserPath}/individuals`,
+  organisation: `${hmrcCreateUserPath}/organisations`,
+  agent: `${hmrcCreateUserPath}/agents`
 };
 
 const app = express();
 
-// set the view engine to ejs
 app.set('view engine', 'ejs');
 
 const log = new (winston.Logger)({
@@ -28,22 +39,17 @@ const log = new (winston.Logger)({
   ]
 });
 
-// home-page route
 app.get('/', (req, res) => {
   res.render('index', {
-    endpoints: endpoints,
+    endpoints: hmrcEndpoints,
   });
 });
 
-// Call an application-restricted endpoint
-app.get("/createTestUser",(req,res) => {
+app.get(appCreateUserEndpoint, (req,res) => {
   const type = req.query.type;
 
-  callApi(endpoints[type], res, serverToken);
+  callApi(hmrcEndpoints[type], res, serverToken);
 });
-
-
-// Helper functions
 
 const callApi = (resource, res, bearerToken) => {
   const acceptHeader = 'application/vnd.hmrc.1.0+json';
